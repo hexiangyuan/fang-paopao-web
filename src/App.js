46608eed94eece2b50fb33a50e9bb287862e85b6
何祥源 <xiangyuan.he@aihuishou.com>
2021-03-15 14:41:18 +0800

    download app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import bg from './assets/bg.jpg';
+import ele1 from './assets/section1-element.png';
+import phoneMain from './assets/section-1-main.png';
+import wxBg from './assets/wx-tips.png';
+import icon from './assets/app-icon.png';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+
+  const [isWx,setIsWx] = useState(false)
+
+  useEffect(()=>{
+    const isWeiXin = navigator.userAgent.toLowerCase().indexOf('micromessenger') > -1 ? true : false
+    setIsWx(isWeiXin)
+  },[])
+
+  function checkIsIOS () {
+    let agent = (navigator.userAgent || navigator.vendor || window.opera)
+
+    if (agent != null) {
+
+      let agentName = agent.toLowerCase()
+
+      if (/android/i.test(agentName)) {
+
+      return false
+
+      } else if (/iphone/i.test(agentName)) {
+
+       return true
+
+      }
+      return false
+    }else{
+      return false
+    }
+  }
+
+  function downloadApp () {
+   const isIOS =  checkIsIOS()
+    if(!isIOS){
+      alert("亲爱的苹果用户，我们暂时不支持苹果 app 的下载。请您用安卓手机更进行体验。")
+    }else{
+      window.location.href = "https://cdn-ll.qindazheng.com/xs/android/new/xiuse-20210127-v2.30-e1267ac32931a1e9b10733e2ed601247.apk"
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-bg">
+        <img src={bg} className="App-bg-img"/>
+        <img src={ele1} className="bg-ele1"/>
+        <div className="App-bg-icon">
+          <img src={icon} height={72} width={72}/>
+          <p className="p-app-name">方泡泡</p>
+        </div>
+        {isWx && <img src={wxBg} className="App-wx-img"/>}
+      </div>
+      <div className="App-phone">
+        <img src={phoneMain} width={"50%"}/>
+      </div>
+      <button className="App-download"
+              type={"submit"}
+     onClick={downloadApp}
+      >立即下载</button>
     </div>
   );
 }
