12c1b2ec8f1b83ddfed2b84ec4b365e50701684a
何祥源 <xiangyuan.he@aihuishou.com>
2021-03-29 21:40:51 +0800

    feat: ios 提审

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import AboutUs from "./about/AboutUs";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <div>
         <Switch>
-      <Route path="/">
-        <App></App>
-      </Route>
+          <Route exact path="/app/about" component={AboutUs}>
+          </Route>
         </Switch>
       </div>
     </Router>
